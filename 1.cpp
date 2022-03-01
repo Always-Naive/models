623a81c775ecb54fec3c214accb4b26c03eebc8b
Always-Naive <97138029+Always-Naive@users.noreply.github.com>
2022-03-01 17:42:41 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -762,3 +762,442 @@ void LineMatch(cv::Mat& left_img, cv::Mat& right_img, const float& baseline, con
 	
 	*/
 }
+
+
+
+
+
+#include iostream
+#includeiomanip
+#includefstream
+#includeunordered_map
+#includemath.h
+#include opencv2corecore.hpp
+#include opencv2highguihighgui.hpp
+#include opencv2imgprocimgproc.hpp
+#include vector
+#include string
+
+
+declaration
+struct EV_VAL_VEC {
+	double nx, ny;nx、ny为最大特征值对应的特征向量
+};
+
+cvMat PreProcessImage(cvMat& img);
+
+cvMat GenerateMask(cvMat& diffImage);
+
+void SetFilterMat(cvMat& Dx, cvMat& Dy, cvMat&Dxx, cvMat&Dyy, cvMat&Dxy, int size_m);
+
+bool isMax(int i, int j, cvMat& img, int dx, int dy);
+
+void StegerLine(cvMat&img0, stdvectorcvPoint2d&sub_pixel, int size_m, int index);
+
+void LineMatch(cvMat& left_img, cvMat& right_img, const float& baseline, const float& focal_length, stdvectorstdvectorcvPoint2d& res);
+
+stdvectorcvPoint3d Img2PointCld(stdvectorstdpaircvPoint2d, cvPoint2d& matched_points, const float cameraParams);
+
+bool SavePointCloud(const char filename, const int size, const double verts_ptr);
+
+bool ReadCameraParameters(const char camera_file, float cameraParams);
+
+main
+int main()
+{
+
+	cvMat img1 = cvimread(cam1.BMP, 0);
+	cvMat img2 = cvimread(cam2.BMP, 0);
+
+	if (img1.empty() img2.empty())
+	{
+		printf( Error opening imagen);
+		return EXIT_FAILURE;
+	}
+	diffimage
+	cvMat result;
+	cvMat source1 = img1.clone();
+	cvMat source2 = img2.clone();
+	cvabsdiff(source1, source2, result);
+
+	crop left image and right image
+	cvRect Left = cvRect(0, 0, result.cols  2, result.rows);
+	cvRect Right = cvRect(result.cols  2, 0, result.cols  2, result.rows);
+	cvMat left_image = result(Left);
+	cvMat right_image = result(Right);
+
+	line extract and line match
+	float baseline;
+	float focal_length;
+	stdvectorstdvectorcvPoint2d res_1;
+	LineMatch(left_image, right_image, baseline, focal_length, res_1);
+
+	return 0;
+}
+
+
+
+
+functions
+
+cvMat PreProcessImage(cvMat& img)
+{
+	cvMat src = img.clone();
+	create Mask
+	cvMat mask = GenerateMask(src);
+	return mask;
+}
+
+cvMat GenerateMask(cvMat& diffImage)
+{
+	cvMat mask = diffImage.clone();
+	stdvectorint x;
+	intensity filter sliding window size 5
+	for (int i = 0; i  mask.rows; i++)
+	{
+		int max_x = 0;
+		int current_intensity = 0;
+		for (int j = 0; j  mask.cols; j++)
+		{
+			int r_intensity;
+			r_intensity = mask.atuint8_t(i, j + 4) + mask.atuint8_t(i, j + 3) + mask.atuint8_t(i, j) + mask.atuint8_t(i, j + 1) + mask.atuint8_t(i, j + 2);
+
+			if (r_intensity  current_intensity)
+			{
+				max_x = j + 2;
+				current_intensity = r_intensity;
+			}
+
+		}
+		stdcout  max_x1  stdendl;
+		int range = max_x;
+		int max_in_range = 0;
+
+		x.push_back(max_x);
+
+	}
+	change useless points' intensity to 0
+	for (int i = 0; i  mask.rows; i++)
+	{
+		for (int k = 0; k  mask.cols; k++)
+		{
+			if (abs(k - x[i])  6)
+			{
+				mask.atuint8_t(i, k) = 0;
+
+			}
+		}
+	}
+	return mask;
+}
+
+void SetFilterMat(cvMat& Dx, cvMat& Dy, cvMat&Dxx, cvMat&Dyy, cvMat&Dxy, int size_m)radius  2= size 
+{
+	if (size_m % 2 == 0  size_m == 1) stdcout  size is illegal !!  stdendl;
+	cvMat m1 = cvMatzeros(cvSize(size_m, size_m), CV_64F);Dx
+	cvMat m2 = cvMatzeros(cvSize(size_m, size_m), CV_64F);Dy
+	cvMat m3 = cvMatzeros(cvSize(size_m, size_m), CV_64F);Dxx
+	cvMat m4 = cvMatzeros(cvSize(size_m, size_m), CV_64F);Dyy
+	cvMat m5 = cvMatzeros(cvSize(size_m, size_m), CV_64F);Dxy
+
+	for (int i = 0; i  size_m  2; i++)
+	{
+		m1.row(i) = 1;
+		m2.col(i) = 1;
+		m3.row(i) = -1;
+		m4.col(i) = -1;
+	}
+	for (int i = size_m  2 + 1; i  size_m; i++)
+	{
+		m1.row(i) = -1;
+		m2.col(i) = -1;
+		m3.row(i) = -1;
+		m4.col(i) = -1;
+	}
+	m3.row(size_m  2) = (size_m  2)  2;
+	m4.col(size_m  2) = (size_m  2)  2;
+	m5.row(size_m  2) = 1;
+	m5.col(size_m  2) = 1;
+	m5.atdouble(size_m  2, size_m  2) = -(size_m  2)  4;
+	if (size_m == 5) m5 = (cvMat_double(5, 5)  0, 0, 1, 0, 0, 0, 1, 2, 1, 0, 1, 2, -16, 2, 1, 0, 1, 2, 1, 0, 0, 0, 1, 0, 0);
+	Dx = m2;
+	Dy = m1;
+	Dxx = m4;
+	Dyy = m3;
+	Dxy = m5;
+	stdcout  Dx  stdendl;
+	stdcout  Dy  stdendl;
+	stdcout  Dxx  stdendl;
+	stdcout  Dyy  stdendl;
+	stdcout  Dxy  stdendl;
+	return;
+}
+
+bool isMax(int i, int j, cvMat& img, int dx, int dy)在法线方向上是否为极值
+{
+	double val = img.atdouble(j, i);
+	double max_v = stdmax(img.atdouble(j + dy, i + dx), img.atdouble(j - dy, i - dx));
+	if (val = max_v) return true;
+	else return false;
+}
+
+void StegerLine(cvMat&img0, stdvectorcvPoint2d&sub_pixel, int size_m, int index)
+{
+
+
+	cvMat img;
+	cvtColor(img0, img0, CV_BGR2GRAY);
+	img = img0.clone();
+
+	高斯滤波
+	img.convertTo(img, CV_64FC1);
+	GaussianBlur(img, img, cvSize(3, 3), 0.9, 0.9);
+
+	一阶偏导数
+	cvMat m1, m2;
+	二阶偏导数
+	cvMat m3, m4, m5;
+	SetFilterMat(m1, m2, m3, m4, m5, size_m);
+
+	stdcout  m5  stdendl;
+
+	cvMat dx, dy;
+	filter2D(img, dx, CV_64FC1, m1);
+	filter2D(img, dy, CV_64FC1, m2);
+
+	cvMat dxx, dyy, dxy;
+	filter2D(img, dxx, CV_64FC1, m3);
+	filter2D(img, dyy, CV_64FC1, m4);
+	filter2D(img, dxy, CV_64FC1, m5);
+
+	hessian矩阵
+	int imgcol = img.cols;
+	int imgrow = img.rows;
+	stdvectordouble Pt;
+	stdstring filename = point_ + stdto_string(index) + .txt;
+	stdofstream points(filename, stdiosout);
+	for (int i = 0; i  imgcol - 1; i++)
+	{
+		for (int j = 0; j  imgrow - 1; j++)
+		{
+			double pixel_val = img.atdouble(j, i);
+			if (img.atdouble(j, i)  10)            需要确定ROI！！！
+			{
+				cvMat hessian(2, 2, CV_64FC1);
+				hessian.atdouble(0, 0) = dxx.atdouble(j, i);
+				hessian.atdouble(0, 1) = dxy.atdouble(j, i);
+				hessian.atdouble(1, 0) = dxy.atdouble(j, i);
+				hessian.atdouble(1, 1) = dyy.atdouble(j, i);
+
+				cvMat eValue;
+				cvMat eVectors;
+				eigen(hessian, eValue, eVectors);
+
+				double nx, ny;
+				double EV_max = 0;特征值
+				double EV_min = 0;
+				if (fabs(eValue.atdouble(0, 0)) = fabs(eValue.atdouble(1, 0)))  求特征值最大时对应的特征向量
+				{
+					nx = eVectors.atdouble(0, 0);大的特征值对应的特征向量
+					ny = eVectors.atdouble(0, 1);
+					EV_max = stdmax(eValue.atdouble(0, 0), eValue.atdouble(1, 0));
+					EV_min = stdmin(eValue.atdouble(0, 0), eValue.atdouble(1, 0));
+				}
+				else
+				{
+					nx = eVectors.atdouble(1, 0);大的特征值对应的特征向量
+					ny = eVectors.atdouble(1, 1);
+					EV_max = stdmax(eValue.atdouble(0, 0), eValue.atdouble(1, 0));
+					EV_min = stdmin(eValue.atdouble(0, 0), eValue.atdouble(1, 0));
+				}
+
+				t taylor expansion
+				double a = nx  nxdxx.atdouble(j, i) + 2  nxnydxy.atdouble(j, i) + ny  nydyy.atdouble(j, i);
+				double b = nx  dx.atdouble(j, i) + ny  dy.atdouble(j, i);
+				double t = -b  a;
+
+				int dx;
+				int dy;
+
+				if (abs(nx) = 2  abs(ny)) dx = 1, dy = 0;垂直
+				else if (abs(ny) = 2  abs(nx)) dx = 0, dy = 1;水平
+				else if (nx  0) dx = 1, dy = 1;
+				else if (ny  0) dx = -1, dy = -1;
+
+				if (i  0 && j  0 && isMax(i, j, img, dx, dy))
+					if(EV_min-120)
+				{
+					double temp1 = t  nx;
+					double temp2 = t  ny;
+					if (fabs(tnx) = 0.5 && fabs(tny) = 0.5)(x + t  Nx, y + t  Ny)为亚像素坐标
+					{
+						Pt.push_back(i + t  nx);
+						Pt.push_back(j + t  ny);
+						if (!points.is_open())
+						{
+							stdcout  cannot open file  stdendl;
+							break;
+						}
+						else
+							points  i + t  nx  ,   j + t  ny  stdendl;
+					}
+				}
+			}
+		}
+	}
+	stdcout  points written to   filename  stdendl;
+	points.close();
+	for (int k = 0; k  Pt.size()  2; k++)
+	{
+		cvPoint2d rpt;
+		rpt.x = Pt[2  k + 0];
+		rpt.y = Pt[2  k + 1];
+		sub_pixel.push_back(rpt);
+	}
+	
+	cvMat Img0 = cvimread(cam1.BMP, 1);
+	for (unsigned int i = 0; i  sub_pixel.size(); i++) {
+		cvcircle(Img0, cvPoint2d(sub_pixel[i].x, sub_pixel[i].y), 0.3, cvScalar(0, 200, 0));
+	}
+	for debug
+	cvimshow(result, Img0);
+	cvwaitKey(0);
+	
+}
+
+
+void LineMatch(cvMat& left_img, cvMat& right_img, const float& baseline, const float& focal_length, stdvectorstdvectorcvPoint2d& res)
+{
+
+	stdvectorcvPoint2d l_points;
+	stdvectorcvPoint2d r_points;
+	preprocess
+	cvMat L = left_img.clone();
+	cvMat R = right_img.clone();
+	cvMat L_res = PreProcessImage(L);
+	cvMat R_res = PreProcessImage(R);
+	cvimshow(aaa, L_res);
+	cvwaitKey(0);
+	cvimshow(bbb, R_res);
+	cvwaitKey(0);
+	steger
+	StegerLine(L_res, l_points, 5, 1);
+	StegerLine(R_res, r_points, 5, 2);
+	cvMat L1(1280, 960, CV_32FC3);
+	cvMat R1(1280, 960, CV_32FC3);
+	for (unsigned int i = 0; i  l_points.size(); i++) {
+		cvcircle(L1, cvPoint2d(l_points[i].x, l_points[i].y), 0.3, cvScalar(0, 200, 0));
+	}
+
+	for (unsigned int i = 0; i  r_points.size(); i++) {
+		cvcircle(R1, cvPoint2d(r_points[i].x, r_points[i].y), 0.3, cvScalar(0, 200, 0));
+	}
+	cvimshow(aaaaa, L1);
+	cvimshow(bbbbb, R1);
+	cvwaitKey(0);
+	res.push_back(l_points);
+	res.push_back(r_points);
+
+	some operation on B and F...
+	
+	
+	 match...
+
+	 disparity...
+	
+	
+	2D-3D
+	float cameraParams[9] = {1.0f};
+	if (!ReadCameraParameters(camera.txt, cameraParams))
+	{
+		编一些值先用一下
+		fx
+		cameraParams[0] = 500;
+		fy
+		cameraParams[4] = 500;
+		ox
+		cameraParams[2] = 640;
+		oy
+		cameraParams[5] = 480;
+		
+		cameraParams[8] = 1;
+		cameraParams[1] = 0;
+		cameraParams[3] = 0;
+		cameraParams[6] = 0;
+		cameraParams[7] = 0;
+	}
+	stdvectorstdpaircvPoint2d,cvPoint2d matched_points;
+	stdvectorcvPoint3d points = Img2PointCld(matched_points, cameraParams);
+
+	save point cloud
+	int num_of_points = points.size();
+	double pointcloud_ptr = new double[num_of_points  3];
+
+	memcpy(pointcloud_ptr, &points[0], sizeof(double) num_of_points 3);
+	SavePointCloud(.points.obj,num_of_points, pointcloud_ptr);
+	delete[] pointcloud_ptr;
+}
+
+bool SavePointCloud(const char filename, const int size, const double verts_ptr)
+{
+	if (verts_ptr = NULL)
+		return false;
+	stdstring file = filename;
+	stdstring path = file.substr(0, file.rfind() + 1);
+	stdstring name = file.substr(file.rfind() + 1, file.rfind(.) - file.rfind() - 1);
+
+	stdofstream fmesh(filename);
+	fmesh  # obj file generated by fengge-niubi  stdendl;
+	fmesh  # Vertices   size  stdendl;
+	
+	for (int i = 0; i  size; i++)
+	{
+		fmesh  v   verts_ptr[i  3 + 0]     verts_ptr[i  3 + 1]   
+			 verts_ptr[i  3 + 2];
+		Red
+		fmesh     1     0     0  n;
+	}
+
+	fmesh.close();
+	return true;
+}
+
+
+stdvectorcvPoint3d Img2PointCld(stdvectorstdpaircvPoint2d, cvPoint2d& matched_points, const float cameraParams)
+{
+	stdvectorcvPoint3d res;
+
+	for (int i = 0; i  matched_points.size(); i++)
+	{
+		cvPoint3d pt;
+		float baseline = 100;
+		float factor = 1;
+		pt.z = baseline  cameraParams[0](matched_points[i].first.x - matched_points[i].second.x);
+
+		pt.x = (matched_points[i].first.x - cameraParams[2]  pt.z)  cameraParams[0];
+		pt.y = (matched_points[i].first.y - cameraParams[2]  pt.z)  cameraParams[4];
+
+		res.push_back(pt);
+	}
+
+	return res;
+}
+
+bool ReadCameraParameters(const char camera_file, float cameraParams) 
+{
+	stdifstream fc;
+	fc.open(camera_file);
+
+	if (!fc.is_open())
+	{
+		stdcout  cannot read camera parameters, using mock params  stdendl;
+		return false;
+	}
+
+	cameraParams[9] = { 1.0f };
+	for (int i = 0; i  9; i++)
+	{	
+		fc  cameraParams[i];
+	}
+	fc.close();
+}
